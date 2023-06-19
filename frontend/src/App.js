2497c7e01c22e51d101bcef5e6eeaa2147b25e81
altinawiwaseem <wasemm@live.com>
2023-06-19 14:06:24 +0200

    create admin dashboard layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import Dashboard from "./components/Admin/Dashboard";
+import Profile from "./components/Admin/Profile";
+import MasterLayout from "./layouts/admin/MasterLayout";
+import { Navigate, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/admin" element={<MasterLayout />}>
+          <Route index element={<Navigate to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
