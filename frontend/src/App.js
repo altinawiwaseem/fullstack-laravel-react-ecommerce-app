66ace400990ea71441612e16053ca484471e02e8
altinawiwaseem <wasemm@live.com>
2023-06-19 15:45:11 +0200

    create navbar and download bootstrap package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import Dashboard from "./components/Admin/Dashboard";
 import Profile from "./components/Admin/Profile";
+import Home from "./components/frontend/Home";
+import Login from "./components/frontend/auth/Login";
+import Register from "./components/frontend/auth/Register";
 import MasterLayout from "./layouts/admin/MasterLayout";
 import { Navigate, Route, Routes } from "react-router-dom";
 
@@ -7,6 +10,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
         <Route path="/admin" element={<MasterLayout />}>
           <Route index element={<Navigate to="dashboard" />} />
           <Route path="dashboard" element={<Dashboard />} />
---
+++ frontend/src/components/frontend/auth/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <button className="btn btn-primary">Register</button>
+    </div>
+  );
+};
+
+export default Register;
